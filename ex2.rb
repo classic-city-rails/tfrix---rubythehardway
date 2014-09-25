5aa3036374e8ff3a31b4b003e3e90ae819ac45cc
Ty Badass Frix <ty.frix@ichor.me>
2014-09-25 19:39:22 -0400

    Ex.2

---
+++ ex2.rb
@@ -0,0 +1,6 @@
+puts "I could have code like this." #and the comment after is ignored
+
+# you can also use a comment to "disable" out a piece of code
+# puts "this wont run"
+
+puts "This will run."
